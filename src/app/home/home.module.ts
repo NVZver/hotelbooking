b653fc3ac37ee83046ae9fbbbf07d456e562fca3
Nikita Zverev <nikita.zverev@konverso.ai>
2019-03-10 13:45:00 +0500

    Added empty home module

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { HomeComponent } from 'src/app/home/home.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  exports: [
+    HomeComponent
+  ],
+  imports: [
+  ],
+  providers: [
+  ]
+})
+export class HomeModule { }
+
